6a44f0c9c5ffdee07427fbd0a57163cfc670ea2e
Nick Allen <nick.allen@dataglide.co>
2026-08-06 16:52:20 +0100

    Complete serialization and deserialization of full Game state to byte array

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::VecDeque, error::Error, io::Write, time::{Duration, Instant}
+    collections::VecDeque, io::Write, time::{Duration, Instant}
 };
 
 use crossterm::{
@@ -11,38 +11,35 @@ use uuid::Uuid;
 
 const GREEN: &str = "\x1b[32m";
 const RED: &str = "\x1b[31m";
-// const YELLOW: &str = "\x1b[33m";
-// const BLUE: &str = "\x1b[34m";
 const RESET: &str = "\x1b[0m";
 
 
 fn main() {
     const WIDTH: i32 = 40;
     const HEIGHT: i32 = 20;
-    let snake_id = Uuid::new_v4();
 
     terminal::enable_raw_mode().unwrap();
-    let mut game = Game::new_game(WIDTH, HEIGHT, snake_id);
+    let mut game = Game::new_game(WIDTH, HEIGHT);
     let mut next_tick = Instant::now();
     loop {
         if let Ok(true) = poll(next_tick - Instant::now()) {
             if let Ok(Event::Key(key)) = read() {
                 match key.code {
                     crossterm::event::KeyCode::Up => {
-                        game.snake.set_incoming_direction(Direction::Up)
+                        game.snake_mut().set_incoming_direction(Direction::Up)
                     }
                     crossterm::event::KeyCode::Right => {
-                        game.snake.set_incoming_direction(Direction::Right)
+                        game.snake_mut().set_incoming_direction(Direction::Right)
                     }
                     crossterm::event::KeyCode::Down => {
-                        game.snake.set_incoming_direction(Direction::Down)
+                        game.snake_mut().set_incoming_direction(Direction::Down)
                     }
                     crossterm::event::KeyCode::Left => {
-                        game.snake.set_incoming_direction(Direction::Left)
+                        game.snake_mut().set_incoming_direction(Direction::Left)
                     }
                     crossterm::event::KeyCode::Char('r') => {
                         if game.state == GameState::GameOver {
-                            game = Game::new_game(WIDTH, HEIGHT, snake_id)
+                            game = Game::new_game(WIDTH, HEIGHT)
                         }
                     }
                     crossterm::event::KeyCode::Char('q') => {
@@ -71,7 +68,7 @@ fn print_game(game: &Game) {
     clear_console();
 
     print_border(game);
-    print_snake(&game.snake);
+    print_snake(&game.snake());
     print_apple(game);
 
     print!("{RESET}");
@@ -170,13 +167,9 @@ fn clear_console() {
 }
 
 impl Game {
-    fn new_game(width: i32, height: i32, snake_id: Uuid) -> Game {
-        let mut game = Game {
-            state: GameState::Running,
-            width: width,
-            height: height,
-            apple: Point { x: 0, y: 0 },
-            snake: Snake {
+    fn new_game(width: i32, height: i32) -> Game {
+        let snake_id = Uuid::new_v4();
+        let snake = Snake {
                     id: snake_id,
                     next_direction: None,
                     body: VecDeque::from_iter(vec![
@@ -202,8 +195,14 @@ impl Game {
                             direction: Direction::Right,
                         },
                     ]),
-                },
-            snakes: vec![],
+                };
+
+        let mut game = Game {
+            state: GameState::Running,
+            width: width,
+            height: height,
+            apple: Point { x: 0, y: 0 },
+            snakes: vec![snake],
             player_snake_id: snake_id
         };
 
@@ -211,6 +210,17 @@ impl Game {
 
         game
     }
+
+    fn snake(&self) -> &Snake {
+        self.snakes.iter().find(|s| s.id == self.player_snake_id)
+            .expect("player snake id not present in game byte array")
+    }
+
+    fn snake_mut(&mut self) -> &mut Snake {
+        self.snakes.iter_mut().find(|s| s.id == self.player_snake_id)
+            .expect("player snake id not present in game byte array")
+    }
+
 
     fn to_byte_array(&self) -> Vec<u8> {
         let mut bytes: Vec<u8> = vec![];
@@ -235,11 +245,7 @@ impl Game {
     }
 
     fn from_byte_array(input: Vec<u8>) -> Game {
-        let state = match input[0] {
-            0 => GameState::Running,
-            1 => GameState::GameOver,
-            invalid_state => panic!("{} not a valid state", invalid_state)
-        };
+        let state = GameState::from_byte(input[0]);
 
         let width = i32::from_le_bytes(input[1..5].try_into().unwrap());
         let height = i32::from_le_bytes(input[5..9].try_into().unwrap());
@@ -247,6 +253,7 @@ impl Game {
         let apple = Point::from_byte_array(input[9..17].to_vec());
 
         let player_snake_id = Uuid::from_bytes_le(input[17..33].try_into().unwrap());
+
         let number_of_snakes = i32::from_le_bytes(input[33..37].try_into().unwrap());
 
         let mut snakes: Vec<Snake> = vec![];
@@ -257,12 +264,8 @@ impl Game {
 
             snakes.push(Snake::from_byte_array(snake_bytes));
 
-            offset += (snake_length + 4) as usize; // length of snake in bytes + the byte that says how long
-                                        // the snake is
+            offset += snake_length as usize + 4 as usize; // move forward the length of snake data + extra 4 bytes for length info of next snake
         }
-
-        let snake = snakes.into_iter().find(|s| s.id == player_snake_id)
-            .expect("player snake id not present in game byte array");
 
         Game {
             state,
@@ -270,8 +273,7 @@ impl Game {
             height,
             apple,
             player_snake_id,
-            snake,
-            snakes: vec![]
+            snakes: snakes
         }
     }
 
@@ -280,7 +282,8 @@ impl Game {
             return;
         }
 
-        let apple_eaten = self.snake.update(&self.apple);
+        let apple = self.apple;
+        let apple_eaten = self.snake_mut().update(&apple);
         if apple_eaten {
             self.apple = self.find_empty_position();
         }
@@ -288,6 +291,10 @@ impl Game {
         if self.is_game_over() {
             self.state = GameState::GameOver;
         }
+
+        // This is unecessary just preparing for communication with server
+        let byte_array = self.to_byte_array();
+        *self = Game::from_byte_array(byte_array);
     }
 
     fn find_empty_position(&self) -> Point {
@@ -302,20 +309,20 @@ impl Game {
     }
 
     fn is_game_over(&self) -> bool {
-        if self.snake.head_position().x >= self.width
-            || self.snake.head_position().x < 0
-            || self.snake.head_position().y >= self.height
-            || self.snake.head_position().y < 0
+        if self.snake().head_position().x >= self.width
+            || self.snake().head_position().x < 0
+            || self.snake().head_position().y >= self.height
+            || self.snake().head_position().y < 0
         {
             return true;
         }
 
         if self
-            .snake
+            .snake()
             .body
             .iter()
             .skip(1)
-            .any(|body_part| &body_part.point == self.snake.head_position())
+            .any(|body_part| &body_part.point == self.snake().head_position())
         {
             return true;
         }
@@ -325,7 +332,6 @@ impl Game {
 }
 
 struct Game {
-    snake: Snake,
     snakes: Vec<Snake>,
     player_snake_id: Uuid,
     apple: Point,
@@ -334,7 +340,7 @@ struct Game {
     height: i32,
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 #[repr(u8)]
 enum GameState {
     Running = 0,
@@ -342,15 +348,16 @@ enum GameState {
 }
 
 impl GameState {
-    fn from_byte(state: u8) -> Result<GameState, Box<dyn Error>> {
+    fn from_byte(state: u8) -> GameState {
         match state {
-            0 => Ok(GameState::Running),
-            1 => Ok(GameState::GameOver),
-            _ => Err("game state can only be 0 or 1".into())
+            0 => GameState::Running,
+            1 => GameState::GameOver,
+            _ => panic!("game state can only be 0 or 1")
         }
     }
 }
 
+#[derive(Debug) ]
 struct Snake {
     id: Uuid,
     next_direction: Option<Direction>,
@@ -416,27 +423,29 @@ impl Snake {
     }
 
     fn from_byte_array(input: Vec<u8>) -> Snake {
-        let snake_id = Uuid::from_bytes_le(input[..16].try_into().expect("failed to get snake id from bytes"));
-        let next_direction = match input[5] {
-            0 => Direction::Up,
-            1 => Direction::Right,
-            2 => Direction::Down,
-            3 => Direction::Left,
+        let snake_id = Uuid::from_bytes_le(input[0..16].try_into().expect("failed to get snake id from bytes"));
+
+        let next_direction = match input[16] {
+            0 => Some(Direction::Up),
+            1 => Some(Direction::Right),
+            2 => Some(Direction::Down),
+            3 => Some(Direction::Left),
+            4 => None,
             invalid_state => panic!("{} not a valid state", invalid_state)
         };
 
-        let body_part_count = i32::from_le_bytes(input[6..10].try_into().unwrap());
+        let body_part_count = i32::from_le_bytes(input[17..21].try_into().unwrap());
 
         let mut body_parts: VecDeque<BodyPart> = VecDeque::new();
         let mut offset: usize = 0;
         for _ in 0..body_part_count {
-            body_parts.push_back(BodyPart::from_byte_array(input[offset..10].to_vec()));
-            offset += 10;
+            body_parts.push_back(BodyPart::from_byte_array(input[21+offset..30+offset].to_vec()));
+            offset += 9;
         }
 
         Snake {
             id: snake_id,
-            next_direction: Option::Some(next_direction),
+            next_direction,
             body: body_parts,
         }
     }
@@ -445,17 +454,24 @@ impl Snake {
         let mut bytes: Vec<u8> = vec![];
 
         bytes.extend(&self.id.to_bytes_le());
-        bytes.push(*self.direction() as u8);
+        match self.next_direction {
+            Some(direction) => bytes.push(direction as u8),
+            None => bytes.push(4 as u8)
+        }
         let body_part_count: &i32 = &self.body.len().try_into().expect("body parts didn't fit into i32");
         bytes.extend(body_part_count.to_le_bytes());
         for part in &self.body {
             bytes.extend(part.to_byte_array());
         }
 
-        bytes
+        let snake_array_length = bytes.len()as i32;
+        let mut result = snake_array_length.to_le_bytes().to_vec();
+        result.extend(bytes);
+        result
     }
 }
 
+#[derive(Debug)]
 struct BodyPart {
     point: Point,
     direction: Direction,
@@ -489,7 +505,7 @@ impl BodyPart {
 }
 
 #[repr(u8)]
-#[derive(Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Direction {
     Up,
     Right,
@@ -500,7 +516,7 @@ enum Direction {
 impl Point {
     fn from_byte_array(input: Vec<u8>) -> Point {
         let x = i32::from_le_bytes(input[0..4].try_into().unwrap());
-        let y = i32::from_le_bytes(input[0..4].try_into().unwrap());
+        let y = i32::from_le_bytes(input[4..8].try_into().unwrap());
 
         Point {
             x,
@@ -518,8 +534,41 @@ impl Point {
     }
 }
 
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_serialize_and_deserialize_game_to_byte_array() {
+        let game = Game::new_game(10, 10);
+
+        let serialized_game = game.to_byte_array();
+
+        let deserizlied_game = Game::from_byte_array(serialized_game);
+
+        assert_eq!(game.height, deserizlied_game.height);
+        assert_eq!(game.width, deserizlied_game.width);
+        assert_eq!(game.state, deserizlied_game.state);
+        assert_eq!(game.apple, deserizlied_game.apple);
+        assert_eq!(game.player_snake_id, deserizlied_game.player_snake_id);
+        assert_eq!(game.snakes.len(), deserizlied_game.snakes.len());
+        assert_eq!(game.snakes[0].id, deserizlied_game.snakes[0].id);
+        assert_eq!(game.snakes[0].body.len(), deserizlied_game.snakes[0].body.len());
+        assert_eq!(game.snakes[0].direction(), deserizlied_game.snakes[0].direction());
+        assert_eq!(game.snakes[0].head_position(), deserizlied_game.snakes[0].head_position());
+        assert_eq!(game.snakes[0].body[0].point, deserizlied_game.snakes[0].body[0].point);
+        assert_eq!(game.snakes[0].body[0].direction, deserizlied_game.snakes[0].body[0].direction);
+        assert_eq!(game.snakes[0].body[1].point, deserizlied_game.snakes[0].body[1].point);
+        assert_eq!(game.snakes[0].body[1].direction, deserizlied_game.snakes[0].body[1].direction);
+        assert_eq!(game.snakes[0].body[2].point, deserizlied_game.snakes[0].body[2].point);
+        assert_eq!(game.snakes[0].body[2].direction, deserizlied_game.snakes[0].body[2].direction);
+
+        assert_eq!(game.to_byte_array(), deserizlied_game.to_byte_array());
+    }
 }
